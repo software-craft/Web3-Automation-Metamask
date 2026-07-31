3552f961e1aecf9356a6c887c2ca68014ea718f4
Leo <leonardoiglesias74@gmail.com>
2026-07-31 08:23:08 -0300

    Synpressinit

---
+++ test/e2e/specs/test.cy.js
@@ -0,0 +1,6 @@
+describe("init setup", () => {
+    it("is able to go tho the baseUrl", () => {
+        cy.visit("https://testnet.rarible.com")
+        cy.wait(90000000000000000000000000000)
+    })
+})
